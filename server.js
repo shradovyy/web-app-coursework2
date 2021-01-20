53976ef00f75bf62c8ac27ea6d8a8458b9818a39
shradovyy <shradovyy@gmail.com>
2021-01-20 18:41:19 +0200

    main commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    console.log("Requested url: "+ req.url);
+    next();
+});
+
+
+// app.use((req, res, next) => {
+//     console.log("image exists");
+//     next();
+// });
+
+
+//connect to db
+let db;
+MongoClient.connect('mongodb+srv://root:superhardpassword@cluster0.lu2f0.mongodb.net/coursework2?retryWrites=true&w=majority', (err, client) => {
+    db = client.db('coursework2');
+});
+
+
+app.get('/lessons', (req, res, next) => {
+    db.collection('lessons').find({}).toArray((e, results) => {
+            if (e) return next();
+            res.json(results);
+    });
+});
+
+
+
+app.use((req, res) => {
+    res.status(404).json({'error': true, 'message': 'something went wrong'});
+});
+
+app.listen(3000);
+
+
+// let publicPath = path.resolve(__dirname, 'public');
+// app.use(express.static(publicPath));
