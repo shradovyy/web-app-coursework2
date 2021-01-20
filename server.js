36d0557e8a4ea94accc0152fa137d89ce5b2abb3
shradovyy <shradovyy@gmail.com>
2021-01-20 20:30:17 +0200

    Added lessons route

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const path = require("path");
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+const ObjectID = require('mongodb').ObjectID;
 
+app.use(express.json());
 
 app.use((req, res, next) => {
     console.log("Requested url: "+ req.url);
     next();
 });
-
 
 // app.use((req, res, next) => {
 //     console.log("image exists");
@@ -17,19 +18,21 @@ app.use((req, res, next) => {
 // });
 
 
-//connect to db
+// connect to db
 let db;
 MongoClient.connect('mongodb+srv://root:superhardpassword@cluster0.lu2f0.mongodb.net/coursework2?retryWrites=true&w=majority', (err, client) => {
     db = client.db('coursework2');
 });
 
 
+// GET route to get a list of lessons
 app.get('/lessons', (req, res, next) => {
     db.collection('lessons').find({}).toArray((e, results) => {
-            if (e) return next();
-            res.json(results);
+        if (e) return next();
+        res.json(results);
     });
 });
+
 
 
 app.use((req, res) => {
