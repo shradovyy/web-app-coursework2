adc5f97b041f5a1903346d749a2552a6607d6d25
shradovyy <shradovyy@gmail.com>
2021-01-21 20:35:58 +0200

    ...

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "*");
     next();
 });
