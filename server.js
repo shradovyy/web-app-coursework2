692239ddfa5fafc24b32c57eb6ffc5fa1acd8b25
shradovyy <shradovyy@gmail.com>
2021-01-21 20:47:36 +0200

    Added CORS

--- server.js
+++ server.js
@@ -14,14 +14,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-});
-
-
 // app.use((req, res, next) => {
 //     console.log("image exists");
 //     next();
